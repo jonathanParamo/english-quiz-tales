9c1dd90f5f7ee4df8b62b1233aa9d6e9d90af78f
jonathanPramo <jonathan.guaydia@devnavigate.com>
2025-10-26 00:00:36 -0500

    feat: add global prefix and Mongo URI validation

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   app.use(cookieParser());
 
